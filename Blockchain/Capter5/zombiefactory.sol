136e29f7c9215f02b8e958a5c181d466ffebc46b
masaya_nakamura <m.nakamura.likebook@gmail.com>
2022-03-03 00:50:27 +0900

    ERC721トークンとメソッドの実装

--- Blockchain/Capter5/zombiefactory.sol
+++ Blockchain/Capter5/zombiefactory.sol
@@ -1,4 +1,4 @@
-// cryptozombies chapter2
+// cryptozombies chapter5
 // https://share.cryptozombies.io/jp/lesson/1/share/yanae?id=Y3p8MTY0MTU1
 pragma solidity ^0.4.19;
 
@@ -22,6 +22,8 @@ contract ZombieFactory is Ownable {
         uint dna;
         uint32 level;
         uint32 readyTime;
+        uint16 winCount;
+        uint16 lossCount;
     }
 
     Zombie[] public zombies;
@@ -36,7 +38,8 @@ contract ZombieFactory is Ownable {
      * _dna  : 遺伝子情報
      ***********************/
     function _createZombie(string _name, uint _dna) internal {
-        uint id = zombies.push(Zombie(_name, _dna, 1, uint32(now + cooldownTime))) - 1;
+
+        uint id = zombies.push(Zombie(_name, _dna, 1, uint32(now + cooldownTime), 0, 0)) - 1;
         zombieToOwner[id] = msg.sender;
         ownerZombieCount[msg.sender]++;
         NewZombie(id, _name, _dna);
